1a3c17b3176dfd65f1bd5d12a8bb2ed3171bf3ff
Peter Phenow <peter.phenow@gmail.com>
2021-05-19 12:01:21 -0500

    about section completed

--- src/mock/data.js
+++ src/mock/data.js
@@ -12,16 +12,21 @@ export const heroData = {
   title: 'Hello, my name is',
   name: 'Peter Phenow',
   subtitle: "I'm a Full-Stack Web Developer.",
-  cta: 'Know more',
+  cta: 'Know more', // button wording
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'tinyprofilecropped.jpg',
+  paragraphOne: `I am a driven Full Stack Web Developer with a background in accounting and finance 
+    and a Full-Stack Certificate from the University of Minnesota.`,
+  paragraphTwo: `During the boot camp, I worked on multiple group projects, honing skills in Node.js, 
+    React, Express, and MongoDB. I recently collaborated with a team of 5 to build an application 
+    from start to finish and demo it to a group of industry professionals with positive reviews.`,
+  paragraphThree: `I enjoy collaborating and working on a diverse team while solving problems. Looking
+    to bring my knowledge and technical skills to an organization while focusing on continuous learning.`,
+  resume:
+    'https://docs.google.com/document/d/1rz1dGKAQeaOQc6Mn3oCUK7bMIODQZIcQ9BBk7SYH8lM/edit?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
